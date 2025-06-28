25154564e48d6ac9b0ca8ab944a4904b0ed57841
ImVladislav <103652143+ImVladislav@users.noreply.github.com>
2025-06-28 12:42:50 +0300

    Update Section2.tsx

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -42,7 +42,7 @@ const Section2 = () => {
       </Box>
       <Box
         component='a'
-        href='https://x.com/Pump_Plushies'
+        href='https://x.com/PumpPlushies'
         sx={{
           img: {
             transition: "0.5s",
