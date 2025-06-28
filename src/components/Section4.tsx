1e7c199834189dc32064256a022977377a069a24
Vladislav <nafanya102@gmail.com>
2025-06-28 07:25:16 +0300

    Update Section4.tsx

--- src/components/Section4.tsx
+++ src/components/Section4.tsx
@@ -102,7 +102,7 @@ const Section4 = () => {
             </Box>
             <Box
               component='a'
-              href='https://x.com/PumpPlushies'
+              href='https://x.com/Pump_Plushies'
               sx={{
                 img: {
                   transition: "0.5s",
